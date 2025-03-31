f69dfabfc2401468a99e1b120acdbc2bd38d160e
Samir Hashimov <166752973+samirrhashimov@users.noreply.github.com>
2025-03-31 16:39:16 +0000

    8.2

--- script.js
+++ script.js
@@ -223,5 +223,6 @@ function cancelNote() {
 }
 
 document.getElementById("menu-btn").addEventListener("click", function () {
-    let menu = document.getElementById("menu").style.display = "block";;
-    
+    let menu = document.getElementById("menu");
+    menu.classList.toggle("hidden");
+});
