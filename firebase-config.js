febeb1c10865b4b0de21b5f8a7ad30e9f83ba9a0
Samir Hashimov <166752973+samirrhashimov@users.noreply.github.com>
2025-05-04 08:34:02 +0000

    15.5.1

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,15 @@
 
 // firebase-config.js
 const firebaseConfig = {
-    apiKey: "AIzaSyDI37ePX-s5JKXqO4r7o9UkQvOchaK4Hr8",
-    authDomain: "notepad-linzaapp.firebaseapp.com",
-    projectId: "notepad-linzaapp",
-    storageBucket: "notepad-linzaapp.firebasestorage.app",
-    messagingSenderId: "625884524445",
-    appId: "1:625884524445:web:9bd53ef63f061e9abd0974",
-    measurementId: "G-631HB9C9W5"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
+
+export default firebaseConfig;
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
